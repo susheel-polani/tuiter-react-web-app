f39a590bfc44266d0518696c298101585a9d81cf
Susheel P S <30873154+susheel98@users.noreply.github.com>
2023-02-19 20:54:18 -0500

    Updates posts.js to handle empty tweets

--- public/tuiter/PostSummaryList/posts.js
+++ public/tuiter/PostSummaryList/posts.js
@@ -6,6 +6,7 @@ export default [
     title:
       "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
     image: "./assets/reactjs-logo.png",
+    tweets: "250K",
   },
   {
     topic: "",
